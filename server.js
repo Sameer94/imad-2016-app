00366d857ce5880c405d8e54888967fc7eb3df61
Sameer94 <sameerbhati96@gmail.com>
2016-10-04 19:53:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,41 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+var htmltemplate=
+
+`
+<html>
+    <head>
+    <title>
+    ${title}
+    <title>
+    <meta name="viewport" content="width-device-width, initial-scale-1"/>
+    <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+        ${heading}
+        </h3>
+        <div>
+        ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+    </div>
+    </body>
+</html>`;
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
