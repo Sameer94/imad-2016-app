4144052e46ad6cf5558ffbccff41bda928b2830f
Sameer94 <sameerbhati96@gmail.com>
2016-12-15 00:23:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+var element =document.getElementById('main-text');
+element.innerHTML='New value';
+
+var img=document.getElementById('madi');
+var marginleft =0;
+
+function moveright(){
+    marginleft=marginleft+1;
+    img.style.marginleft=marginleft + 'px';
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveright,50);
+};
