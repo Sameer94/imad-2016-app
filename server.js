48fa1424d08747c99e07200266c29b070c27e8a8
Sameer94 <sameerbhati96@gmail.com>
2016-12-17 22:50:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,13 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){
+   
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
 
 app.get('/articleone', function (req, res) {
   res.send(createTemplate(articleone));
