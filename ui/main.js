de846d0f4aff22d71a6a741478e0c613bdf9cc82
Sameer94 <sameerbhati96@gmail.com>
2016-12-17 22:05:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,18 @@ button.onclick=function(){
   request.open('GET','http://sameer94.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+//submit 
+var nameInput= document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementbyId('submit_btn');
+submit.onclick= function(){
+    var names= ['name1','name2','name3'];
+    var list='';
+    for(var i=0;i < names.length;i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML=list;
+};
